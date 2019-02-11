530a6a39843bd9e9c88ea6ed79fbbea094894962
Oskar Laska <oskar.laska@orba.co>
2019-02-11 11:37:11 +0100

    Get rid of jQuery

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,21 @@
 import { check } from './check.js';
 
-$(document).ready(() => {
-    $("#form").on('submit', () => {
-        let result = check($("#input").val());
-        $("#result").removeClass(`${result ? 'in' : ''}correct`).addClass(`${!result ? 'in' : ''}correct`);
-        return false;
-    }).on('input', () => {
-        $("#result").removeClass('correct incorrect');
-    })
-});
+document.addEventListener('DOMContentLoaded', () => { 
+  const formElem = document.getElementById('form');
+  const inputElem = document.getElementById('input');
+  const resultElem = document.getElementById('result');
+
+  formElem.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const result = check(inputElem.value);
+
+    resultElem.classList.remove(`${result ? 'in' : ''}correct`);
+    resultElem.classList.add(`${!result ? 'in' : ''}correct`);
+  });
+
+  formElem.addEventListener('input', () => {
+    resultElem.classList.remove('correct', 'incorrect');
+  });
+
+}, false);
