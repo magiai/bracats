54c002434d0f689edc516dd29e434920dae3d6a7
Dominika <dominikaadamska@integree.eu>
2020-08-04 14:04:29 +0200

    finished styles and js for bracats app

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,9 @@
 import { check } from './check.js';
 
 document.addEventListener('DOMContentLoaded', () => { 
+  const checkerElem = document.getElementById('checker-container');
   const formElem = document.getElementById('form');
-  const inputElem = document.getElementById('input');
+  const inputElem = document.getElementById('bracats-input');
   const resultElem = document.getElementById('result');
 
   formElem.addEventListener('submit', (e) => {
@@ -10,12 +11,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const result = check(inputElem.value);
 
-    resultElem.classList.remove(`${result ? 'in' : ''}correct`);
-    resultElem.classList.add(`${!result ? 'in' : ''}correct`);
+    resultElem.style.setProperty("--bg-image", `${result ? 'url(http://localhost:8000/img/correct.jpg)' : 'url(http://localhost:8000/img/incorrect.jpg)'}`);
+    resultElem.ariaLabel = result ? "image of happy cat" : "image of angry cat";
+    checkerElem.style.setProperty("--bg", `${result ? '#85D179': '#5B5B5B'}`);
+    
   });
 
   formElem.addEventListener('input', () => {
-    resultElem.classList.remove('correct', 'incorrect');
+    resultElem.style.setProperty("--bg-image", '');
+    resultElem.ariaLabel = "";
+    checkerElem.style.setProperty("--bg", '#D4D4D4');
   });
 
 }, false);
--- public/js/check.js
+++ public/js/check.js
@@ -1,3 +1,55 @@
-const check = (string) => string === 'cattac';
+const check = (string) => {
+
+  if (string.length === 0) true;
+
+  //check validity of the derived 3-letter words
+  const checkValidity = (bracats) => {
+    
+    const validBracats = {
+      'tac' : 'cat',
+      'TAC' : 'CAT',
+      'tAc' : 'cAt'
+    }
+
+    //return the last word to find the closing bracat
+    const peek = () => {
+
+      if (checkArr.length === 0) {
+        return null;
+      }
+
+      return checkArr[checkArr.length - 1];
+    }
+
+    let checkArr = [];
+
+    for (let i = 0; i < bracats.length; i++) {
+
+      //check if current word matches the last one if so delete it
+      if (validBracats[bracats[i]] === peek()) {
+        checkArr.pop();
+      } else {
+        checkArr.push(bracats[i]);
+      };
+    }
+
+    return checkArr.length === 0
+  }
+  
+  if (string.length %2 === 0 && string.length %3 === 0) {
+    
+    let wordsArr = [];
+    
+    //divide a string to get 3-letter words
+    for (let i=0; i < string.length; i += 3) {
+      wordsArr.push(string.substr(i, 3));
+    }
+  
+    return checkValidity(wordsArr)
+
+  } else {
+    return false;
+  }
+}
 
 export { check };
